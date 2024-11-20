9ace515b496de9b66d078f31a62260803ea61731
dkolesni-prog <63922878+dkolesni-prog@users.noreply.github.com>
2024-11-20 21:56:42 +0300

    iter 1 initial commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,70 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"net/http"
+)
+
+func firstEndpoint(w http.ResponseWriter,
+	r *http.Request,
+	keyLongValueShort map[string]string,
+	keyShortValueLong map[string]string) {
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to read body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	hash := sha256.Sum256(body)
+	hashStr := hex.EncodeToString(hash[:])[:8]
+	keyLongValueShort[string(body)] = hashStr
+	keyShortValueLong[hashStr] = string(body)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(201)
+	w.Write([]byte("http://localhost:8080/" + hashStr + "\n"))
+}
+
+func secondEndpoint(w http.ResponseWriter,
+	r *http.Request,
+	keyShortValueLong map[string]string) {
+
+	id := r.URL.Path[1:]
+
+	long, exists := keyShortValueLong[id]
+	if !exists {
+		http.Error(w, "Short URL not found", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", long)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func main() {
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+
+	var keyLongValueShort = map[string]string{}
+	var keyShortValueLong = map[string]string{}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost && r.URL.Path == "/" {
+			firstEndpoint(w, r, keyLongValueShort, keyShortValueLong)
+		} else if r.Method == http.MethodGet && len(r.URL.Path) > 1 {
+			secondEndpoint(w, r, keyShortValueLong)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	})
+
+	return http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dkolesni-prog/transformer
+
+go 1.22.0
