123e2c8046eca9417c420ee4ff5cca74c11e0ffc
dkolesni-prog <63922878+dkolesni-prog@users.noreply.github.com>
2024-12-21 14:42:42 +0700

    filled logger.go

--- internal/app/logger.go
+++ internal/app/logger.go
@@ -1,1 +1,61 @@
 package app
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var Log *zap.Logger = zap.NewNop()
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	size       int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	size, err := rw.ResponseWriter.Write(b)
+	rw.size += size
+	return size, err
+}
+
+func Initialize(level string) error {
+	lvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return err
+	}
+	cfg := zap.NewProductionConfig()
+	cfg.Level = lvl
+	zl, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+	Log = zl
+	return nil
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ww := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		h.ServeHTTP(ww, r)
+
+		duration := time.Since(start)
+
+		Log.Info("handled request",
+			zap.String("URI", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Int("status", ww.statusCode),
+			zap.Int("size", ww.size),
+			zap.Duration("duration", duration),
+		)
+	})
+}
