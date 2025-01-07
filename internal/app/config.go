f1bfb804d9a3ceb009bd0db8ea7cc76c0aaf81b2
dkolesni-prog <63922878+dkolesni-prog@users.noreply.github.com>
2025-01-07 14:19:08 +0700

    try

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,38 +1,75 @@
-// Internal/app/config.go.
+// // Internal/app/config.go.
+// package app
+//
+// import (
+//
+//	"flag"
+//	"os"
+//	"sync"
+//
+// )
+//
+//	type Config struct {
+//		RunAddr         string
+//		BaseURL         string
+//		FileStoragePath string
+//	}
+//
+// var parseOnce sync.Once
+//
+//	func NewConfig() *Config {
+//		cfg := Config{}
+//
+//		parseOnce.Do(func() {
+//			flag.StringVar(&cfg.RunAddr, "a", ":8080", "address and port to run server")
+//			flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "base URL for shortened links")
+//			flag.StringVar(&cfg.FileStoragePath, "f", "shortener_data.json", "path to file with shortener data")
+//			flag.Parse()
+//		})
+//		if envRunAddr, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+//			cfg.RunAddr = envRunAddr
+//		}
+//		if envBaseURL, ok := os.LookupEnv("BASE_URL"); ok {
+//			cfg.BaseURL = envBaseURL
+//		}
+//		if envFilePath, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
+//			cfg.FileStoragePath = envFilePath
+//		}
+//		cfg.BaseURL = ensureTrailingSlash(cfg.BaseURL)
+//		return &cfg
+//	}
 package app
 
 import (
 	"flag"
 	"os"
-	"sync"
 )
 
 type Config struct {
 	RunAddr         string
-	BaseURL         string
 	FileStoragePath string
+	BaseURL         string
 }
 
-var parseOnce sync.Once
-
 func NewConfig() *Config {
-	cfg := Config{}
+	var filePath string
+	flag.StringVar(&filePath, "f", "", "Path to the file storage")
+	flag.Parse()
 
-	parseOnce.Do(func() {
-		flag.StringVar(&cfg.RunAddr, "a", ":8080", "address and port to run server")
-		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "base URL for shortened links")
-		flag.StringVar(&cfg.FileStoragePath, "f", "shortener_data.json", "path to file with shortener data")
-		flag.Parse()
-	})
-	if envRunAddr, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
-		cfg.RunAddr = envRunAddr
+	// Check environment variables first
+	envFilePath := os.Getenv("FILE_STORAGE_PATH")
+	if envFilePath != "" {
+		filePath = envFilePath
 	}
-	if envBaseURL, ok := os.LookupEnv("BASE_URL"); ok {
-		cfg.BaseURL = envBaseURL
+
+	// Fallback to default if neither flag nor environment variable is set
+	if filePath == "" {
+		filePath = "shortener_data.json"
 	}
-	if envFilePath, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
-		cfg.FileStoragePath = envFilePath
+
+	return &Config{
+		RunAddr:         ":8080",
+		FileStoragePath: filePath,
+		BaseURL:         "http://localhost:8080",
 	}
-	cfg.BaseURL = ensureTrailingSlash(cfg.BaseURL)
-	return &cfg
 }
